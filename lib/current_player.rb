8c746c2fedd8f7188a36de24d3405b71621362b3
Marc Steele <marcsteele2014@gmail.com>
2020-09-30 02:00:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,6 +8,11 @@ def turn_count(board)
     return counter
 end
 
-def current_player
-
+def current_player(board)
+  if turn_count(board) % 2 -= 0
+    return "X"
+  else
+    return "O"
+  end
+  
 end
